e8c1a0463a0889316f73e20c76acc27849d76889
firefly2442 <firefly2442@gmail.com>
2018-01-11 23:23:33 -0700

    update examples

--- config.inc.php
+++ config.inc.php
@@ -1,45 +1,27 @@
 <?php
 
-//change this to reflect the servers that you want to query
-$servers = array(
-	array(
+// change this to reflect the servers that you want to query
+// https://github.com/Austinb/GameQ/wiki/Examples-v3#different-client-and-query-ports
+$servers = [
+	[
 		'id' => 'AlphaSquad Arma 3',
-		'type' => 'armedassault3',
+		'type' => 'arma3',
 		'host' => '207.182.138.242:2302',
 		'options' => [
 			'query_port' => 2303
 		]
-	),
-	array(
-		'id' => 'Kellys Heroes',
-		'type' => 'armedassault3',
-		'host' => '144.76.38.131:2402'
-	),
-	array(
-		'id' => 'TAW',
-		'type' => 'armedassault3',
-		'host' => '64.31.29.82:2402'
-	),
-	array(
-		'id' => 'Arma2 OA Test',
-		'type' => 'armedassault2oa',
-		'host' => '74.91.120.129:2312'
-	),
-	array(
-		'id' => 'United Operations Arma 3',
-		'type' => 'armedassault3',
-		'host' => 'arma3.unitedoperations.net:2402'
-	),
-	array(
-		// https://github.com/Austinb/GameQ/wiki/Examples-v3#different-client-and-query-ports
-		'id' => 'DayZ Test',
-		'type' => 'dayz',
-		'host' => '108.174.57.251:2302',
-		//'options' => [
-			//'query_port' => 6502
-		//]
-	)
-);
+	],
+	//[
+		//'id' => 'Arma 2 OA Test',
+		//'type' => 'armedassault2oa',
+		//'host' => '107.191.44.98:2302'
+	//],
+	//[
+		//'id' => 'DayZ Test',
+		//'type' => 'dayz',
+		//'host' => '198.12.65.82:2302'
+	//]
+];
 
 
 //TODO: currently offline, looking into alternatives
